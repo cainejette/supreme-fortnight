9624e2e3e494831c51bd0736b01828d9b8f60123
Caine Jette <caine@attackpattern.com>
2016-10-15 00:37:50 -0700

    Add logic for command options.

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ______    ______    ______   __  __    __    ______
- /\  == \  /\  __ \  /\__  _\ /\ \/ /   /\ \  /\__  _\
- \ \  __<  \ \ \/\ \ \/_/\ \/ \ \  _"-. \ \ \ \/_/\ \/
- \ \_____\ \ \_____\   \ \_\  \ \_\ \_\ \ \_\   \ \_\
- \/_____/  \/_____/    \/_/   \/_/\/_/  \/_/    \/_/
+'use strict';
 
-
- This is a sample Slack Button application that provides a custom
- Slash command.
-
- This bot demonstrates many of the core features of Botkit:
-
- *
- * Authenticate users with Slack using OAuth
- * Receive messages using the slash_command event
- * Reply to Slash command both publicly and privately
-
- # RUN THE BOT:
-
- Create a Slack app. Make sure to configure at least one Slash command!
-
- -> https://api.slack.com/applications/new
-
- Run your bot from the command line:
-
- clientId=<my client id> clientSecret=<my client secret> PORT=3000 node bot.js
-
- Note: you can test your oauth authentication locally, but to use Slash commands
- in Slack, the app must be hosted at a publicly reachable IP or host.
-
-
- # EXTEND THE BOT:
-
- Botkit is has many features for building cool and useful bots!
-
- Read all about it here:
-
- -> http://howdy.ai/botkit
-
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-/* Uses the slack button feature to offer a real time bot to multiple teams */
 var Botkit = require('botkit');
+const curl = require('curlrequest');
+var google = require('google')
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
@@ -80,41 +41,72 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
     });
 });
 
-
-//
-// BEGIN EDITING HERE!
-//
-
 controller.on('slash_command', function (slashCommand, message) {
-
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
-            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
-            // Otherwise just echo back to them what they sent us.
+        case "/xkcd": 
+            if (message.token !== process.env.VERIFICATION_TOKEN) return;
 
-            // but first, let's make sure the token matches!
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
-
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
-                return;
+            if (message.text === "") {
+                getRandomComic(slashCommand, message);
+            } else if (message.text === "help") {
+                slashCommand.replyPrivate(message, "I try to find a relevant xkcd for you. Try typing `/xkcd standards` to see.");
+            } else {
+                getRelevantComic(slashCommand, message);
             }
 
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
-
             break;
+
         default:
             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
-
+            break;
     }
+});
 
-})
-;
+var getRandomComic = (slashCommand, message) => {
+    curl.request('http://xkcd.com/info.0.json', (err, data) => {
+        if (err) {
+            slashCommand.replyPrivate(message, 'Sorry, I failed: ' + err);
+            return;
+        } 
 
+        var lastComic = JSON.parse(data).num;
+        var randomComic = Math.floor(Math.random() * lastComic)
+
+        postComic(slashCommand, message, 'http://xkcd.com/{0}/info.0.json'.replace('{0}', randomComic));
+    });
+};
+
+var getRelevantComic = (slashCommand, message) => {
+    google('xkcd ' + message.text, (err, data) => {
+        if (err) {
+            slashCommand.replyPrivate(message, 'Sorry, I failed: ' + err);
+            return;
+        }
+
+        var link = data.links[0].href + '/info.0.json';
+        postComic(slashCommand, message, link);
+    });
+};
+
+var postComic = (slashCommand, message, link) => {
+    curl.request(link, (err, data) => {
+        if (err) {
+            slashCommand.replyPrivate(message, 'Sorry, I failed: ' + err);
+            return;
+        }
+
+        var comic = JSON.parse(data);
+        var response = {
+            "attachments": [
+                {
+                    "title": comic.title,
+                    "title_link": 'http://xkcd.com/' + comic.num,
+                    "image_url": comic.img,
+                    "footer": comic.alt,
+                }
+            ]
+        };
+
+        slashCommand.replyPublic(message, response);
+    });
+}
